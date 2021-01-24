28f84f65e1cb3c8b0697cf29457d6fdaf05b998d
restimel <restimel@gmail.com>
2021-01-24 12:13:16 +0100

    introduce Back component

---
+++ src/components/Back.vue
@@ -0,0 +1,32 @@
+<template>
+    <button
+        class="button-back"
+        @click="onclick"
+    >
+        {{textBack}}
+    </button>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+const textBack = '← Retour';
+
+@Options({
+})
+export default class FieldInput extends Vue {
+    private textBack = textBack;
+
+    private onclick() {
+        this.$router.go(-1);
+    }
+}
+</script>
+<style scoped>
+.button-back {
+    width: 100%;
+    color: var(--button-color);
+}
+.button-back:hover {
+    color: var(--button-color-hover);
+}
+</style>
